234b0fef53e8a9dc6b869ef4500cd5a1656131d9
Tim Nordin <74505667+Sidus-96@users.noreply.github.com>
2023-10-23 13:28:32 +0200

    Första_Commit, bygger interface

---
+++ TravelPal_App/Interface/IUser.cs
@@ -0,0 +1,22 @@
+﻿namespace TravelPal_App.Interface
+{
+    public interface IUser
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Country { get; set; }
+        void Iuser(string username, string password, string country);
+
+
+        //public IUser(string username, string password, string country)
+        //{
+        //    Username = username;
+        //    Password = password;
+        //    Country = country;
+        //}
+    }
+
+
+
+
+}
